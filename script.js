c9da932c2be88627dc436957daa50c131203503d
jesciak <jesciak@yahoo.com>
2022-10-12 22:28:06 -0400

    adding and removing CSS styling to certain classes

--- script.js
+++ script.js
@@ -1,5 +1,60 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-//displays current day at top of calendar
+    //displays current day at top of calendar
     $('#currentDay').text(moment().format("dddd MMMM Do"));
-});
+
+
+
+    var nowtTime = moment().format('H'));
+
+
+    $('.time-block').each(function () {
+        var timeBlock = parseInt(s(this).attr('id').split('-')[1]);
+        var
+    
+        if (timeBlock < nowTime) {
+            $(this).addClass("past");
+        }
+        else if (timeBlock === nowTime) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+        }
+        else {
+        
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    });
+
+
+whatHour();
+
+
+//     var displayEl = document.getElementById  ('displayElement');
+//     displayEl.innerHTML= nowtTime;
+    
+//     function timeColor(){
+//         var currentTime = 
+//         var
+//     }
+
+// function addInput ()
+// var textInput = $(`block1,block2, bloc)
+//creating new HTML elements to the DOM 
+
+// const timeBlock =$('.time-block').addClass('row');
+// const textInput =$('<p>').addClass('input');
+
+// timeBlock.append(textInput);
+
+
+// var block1 =
+// var block2 =
+// var block3=
+// var block4=
+// var block5=
+// var block6=
+// var block7=
+// var block8=
+// var block9=
